861c6c6c6f00548ccbe12c074526a4f57f650c8d
hzw517262544 <517262544@qq.com>
2018-10-30 10:24:24 +0800

    app_backer

---
+++ src/main/java/com/bootdo/BootdoApplication.java
@@ -0,0 +1,64 @@
+package com.bootdo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
+@EnableTransactionManagement
+@ServletComponentScan
+@MapperScan("com.bootdo.*.dao")
+@SpringBootApplication
+@EnableCaching
+public class BootdoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootdoApplication.class, args);
+        System.out.println("ヾ(◍°∇°◍)ﾉﾞ    bootdo启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
+                " ______                    _   ______            \n" +
+                "|_   _ \\                  / |_|_   _ `.          \n" +
+                "  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
+                "  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
+                " _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
+                "|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //放行哪些原始域
+        corsConfiguration.addAllowedOrigin("*");
+        //放行哪些原始域(头部信息)
+        corsConfiguration.addAllowedHeader("*");
+        //放行哪些原始域(请求方式)
+        corsConfiguration.addAllowedMethod("*");
+        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+//        corsConfiguration.addExposedHeader("*");
+        corsConfiguration.addExposedHeader("x-auth-token");
+        corsConfiguration.addExposedHeader("x-total-count");
+        //是否发送Cookie信息
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
+    }
+
+    /**
+     * 跨域过滤器
+     *
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+}
---
+++ src/main/java/com/bootdo/app/controller/LoginAppController.java
@@ -0,0 +1,57 @@
+package com.bootdo.app.controller;
+
+import com.bootdo.common.annotation.Log;
+import com.bootdo.common.controller.BaseController;
+import com.bootdo.common.domain.FileDO;
+import com.bootdo.common.domain.Tree;
+import com.bootdo.common.service.FileService;
+import com.bootdo.common.utils.MD5Utils;
+import com.bootdo.common.utils.R;
+import com.bootdo.common.utils.ShiroUtils;
+import com.bootdo.system.domain.MenuDO;
+import com.bootdo.system.domain.UserDO;
+import com.bootdo.system.service.MenuService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/app")
+public class LoginAppController extends BaseController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@PostMapping("/login")
+	@ResponseBody
+	R ajaxLogin(String username, String password) {
+
+		password = MD5Utils.encrypt(username, password);
+		UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+		Subject subject = SecurityUtils.getSubject();
+		try {
+			subject.login(token);
+			return R.ok();
+		} catch (AuthenticationException e) {
+			return R.error("用户或密码错误");
+		}
+	}
+
+	@GetMapping("/logout")
+	@ResponseBody
+	R logout() {
+		ShiroUtils.logout();
+		return R.ok();
+	}
+
+}

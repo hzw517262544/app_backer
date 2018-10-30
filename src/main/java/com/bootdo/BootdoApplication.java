7984ea7b53c314e475b6c1fdc15694daf1e846cc
hzw517262544 <517262544@qq.com>
2018-10-30 11:07:02 +0800

    20181030

--- src/main/java/com/bootdo/BootdoApplication.java
+++ src/main/java/com/bootdo/BootdoApplication.java
@@ -42,7 +42,7 @@ public class BootdoApplication {
         //放行哪些原始域(请求方式)
         corsConfiguration.addAllowedMethod("*");
         //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
-//        corsConfiguration.addExposedHeader("*");
+//        corsConfiguration.addExposedHeader("**");
         corsConfiguration.addExposedHeader("x-auth-token");
         corsConfiguration.addExposedHeader("x-total-count");
         //是否发送Cookie信息
